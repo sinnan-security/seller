850085dd15be55f56308fc452ab05ea9bb60675d
sinnan-security <sinnan.suleman@daraz.pk>
2022-06-14 21:01:12 +0500

    Updates2.0

--- app.py
+++ app.py
@@ -26,18 +26,17 @@ def micro_service(i):
 @app.route('/health', methods=['GET'])
 def health():
 	for i in config:
-		print(i)
 		if i.startswith('micro'):
-			print('%s_flag=micro_service(config["%s"])'%(i,i))
 			exec('%s_flag=micro_service(config["%s"])'%(i,i))
+	x=locals()
 	db=db_query(config['db'],'SELECT VERSION();')
 	return make_response(jsonify({
 		'service_seller':{
 			'CPU_usage':str(psutil.cpu_percent()),'RAM_usage':str(psutil.virtual_memory().percent),'STORAGE_usage':str(psutil.disk_usage('/').percent),			'SQLITE_connection':db,
 			'API_logstash':'To_Be_Implemented',
-			'micro_auth':micro_auth_flag,
-			'micro_payment':micro_payment_flag,
-			'micro_product':micro_product_flag,
+			'micro_auth':x['micro_auth_flag'],
+			'micro_payment':x['micro_payment_flag'],
+			'micro_product':x['micro_product_flag'],
 		}}),200)
 
 @app.route('/api/seller/SomeRoute', methods=['GET'])
