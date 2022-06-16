50c808a87c7a33f55c9f3fa8d838e7e64ff22722
sinnan-security <sinnan.suleman@daraz.pk>
2022-06-16 14:48:55 +0500

    Implement other functionality

--- app.py
+++ app.py
@@ -47,6 +47,12 @@ def SomeFunctionality():
 	response={}
 	logger(request,response)
 	return "<h1>seller service %s</H1>"%(rand)
+	
+@app.route('/api/seller/OtherRoute', methods=['POST'])
+def Otherfunctionality():
+	response={}
+	logger(request,response)
+	return "<h1>Some Response!</H1>"
 
 def logger(request,response):
 	tmp='headers:{'
